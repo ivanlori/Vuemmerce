99befc69bd18c1b9e0edda51fde24967f210d473
ivan lori <ivan.lori@protonmail.com>
2017-12-01 21:35:36 +0100

    eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
       'optionalDependencies': ['test/unit/index.js']
     }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'linebreak-style': 0
   }
 }
