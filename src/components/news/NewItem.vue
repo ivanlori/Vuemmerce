6bbe35d67e0d279acfccaef96cb3d0f2d492fab5
Vitaliy <Nesterenko>
2019-08-08 09:32:10 +0300

    Новости #13

---
+++ src/components/news/NewItem.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <router-link v-bind:to="link">
+      <div class="news-header">{{newItem.title}}</div>
+    </router-link>
+    <div class="news-date">{{date}}</div>
+    
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "newItem",
+  props: ["newItem"],
+
+  data() {
+    return {
+    
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  computed: {
+    link() {
+      return `/news/${this.newItem.id}`;
+    },
+    date() {
+      var date = new Date(this.newItem.date);
+      var dd = date.getDate();
+      var mm = date.getMonth() + 1;
+      var yyyy = date.getFullYear();
+
+      if (dd < 10) {
+        dd = '0' + dd;
+      } 
+      
+      if (mm < 10) {
+        mm = '0' + mm;
+      } 
+      return dd + '.' + mm + '.' + yyyy;
+    }
+  },
+
+  methods: {
+
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .news-header {
+    font-size: 16px;
+    font-weight: 900;
+    text-transform: capitalize;
+    margin-bottom: 16px;
+  }
+
+  .news-date {
+    font-size: 10px;
+    color: rgba(0, 0, 0, 0.4);
+  }
+
+</style>
+
+
---
+++ src/components/news/NewsListContainer.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container news">
+   <h1 class="title">News</h1>
+    <div class="news-list">
+      <div class="card column is-one-quarter" v-for="n in news" :key="n.id">
+        <VmNewItem :newItem="n"></VmNewItem>
+      </div>
+    </div>
+    <div class="all-news">
+      <router-link to="/news">
+        All news
+      </router-link>
+    </div>
+    
+
+        
+      
+      
+    
+    
+  </div>
+</template>
+
+<script>
+
+import VmNewItem from './NewItem';
+
+export default {
+  name: 'newsList',
+  
+  components: { VmNewItem },
+  
+  data () {
+    return {
+      id: '',
+      noProductLabel: 'No product found',
+      productsFiltered: []
+    };
+  },
+
+  computed: {
+    news () {
+      return this.$store.state.news.slice(0, 3);
+    }
+  },
+
+  methods: {
+    
+  }
+
+};
+</script>
+
+<style lang="scss" scoped>
+  .news {
+    margin-bottom: 40px;
+  }
+
+  .news-list {
+    display: flex;
+    justify-content: space-between;
+    
+  }
+
+  .title {
+    text-align: center;
+    margin-top: 20px;
+  }
+  .card {
+    margin: 10px;
+    
+  }
+
+  .all-news {
+    margin-left: 10px;
+  }
+</style>
